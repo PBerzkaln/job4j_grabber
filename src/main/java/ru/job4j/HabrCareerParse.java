40f253250749b220ba40568c0aa21e792e4fea3b
pavelberzkaln <p.berzkaln@gmail.com>
2022-08-07 18:10:06 +0300

    2.1.1. Парсинг https://career.habr.com/vacancies/java_developer?page=1 [#285210]

--- src/main/java/ru/job4j/HabrCareerParse.java
+++ src/main/java/ru/job4j/HabrCareerParse.java
@@ -6,6 +6,8 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HabrCareerParse {
 
@@ -13,26 +15,35 @@ public class HabrCareerParse {
 
     private static final String PAGE_LINK = String.format("%s/vacancies/java_developer", SOURCE_LINK);
 
-    /**Сначала мы получаем страницу (Document).
+    /**В цикле проходим по первым 5 страницам.
+     *Сначала мы получаем страницу (Document).
      *Получаем все вакансии страницы (Elements).
-     *Проходим по каждой вакансии и получаем нужные
-     *элементы (title, link, date).
+     *Получаем нужные элементы (title, link, date).
+     *Сохраняем данные в виде строки в list,
+     *затем в for-each выводим их на консоль.
      * @param args
      * @throws IOException
      */
     public static void main(String[] args) throws IOException {
-        Connection connection = Jsoup.connect(PAGE_LINK);
-        Document document = connection.get();
-        Elements rows = document.select(".vacancy-card__inner");
-        rows.forEach(row -> {
-            Element titleElement = row.select(".vacancy-card__title").first();
-            Element linkElement = titleElement.child(0);
-            String vacancyName = titleElement.text();
-            Element dateElement = row.select(".vacancy-card__date").first();
-            Element date = dateElement.child(0);
-            String[] vacancyDate = String.format("%s", date.attr("datetime")).split("T");
-            String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
-            System.out.printf("%s %s %s%n", vacancyDate[0], vacancyName, link);
-        });
+        List<String> savedPages = new ArrayList<>();
+        for (int i = 1; i <= 5; i++) {
+            String pages = String.format("%s?page=%d", PAGE_LINK, i);
+            Connection connection = Jsoup.connect(pages);
+            Document document = connection.get();
+            Elements rows = document.select(".vacancy-card__inner");
+            rows.forEach(row -> {
+                Element titleElement = row.select(".vacancy-card__title").first();
+                Element linkElement = titleElement.child(0);
+                String vacancyName = titleElement.text();
+                Element dateElement = row.select(".vacancy-card__date").first();
+                Element date = dateElement.child(0);
+                String[] vacancyDate = String.format("%s", date.attr("datetime")).split("T");
+                String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
+                savedPages.add(String.format("%s %s %s", vacancyDate[0], vacancyName, link));
+            });
+        }
+        for (String s : savedPages) {
+            System.out.println(s);
+        }
     }
 }
